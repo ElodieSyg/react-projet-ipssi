7c630b22950653ee4d5797ded798039031732dfa
unknown <cherifoussama1996@gmail.com>
2022-07-01 15:54:25 +0200

    faq

--- front/src/pages/faq/index.js
+++ front/src/pages/faq/index.js
@@ -1,6 +1,30 @@
+import "../../styles/faq.modules.css";
+
 const Faq = () => {
     return (
-        <div>Faq</div>
+        <div className="faq">
+            <h2>FAQ</h2>
+            <div>
+                <h3>Why Should I Use a Travel Agent to Book a Vacation?</h3>
+                <p>A travel agent takes all of the headache out of planning a trip and handles virtually all aspects of your travel, at absolutely no cost to you. The combined experience of the team and the feedback from our thousands of travelers provides an invaluable resource to the traveler planning an important trip.</p>
+            </div>
+            <div>
+                <h3>Can You Work Within a Certain Budget?</h3>
+                <p>Yes, we work within any and all realistic budgets, and an honest sharing of ideas and goals helps us immensely when making recommendations and suggested edits to travel plans.</p>
+            </div>
+            <div>
+                <h3>Does It Cost Money to Get a Quote?</h3>
+                <p>No, there is no fee for our vacation planning services as the travel vendors involved recognize our value and compensate us from the gross amount paid (what you would have paid even without our assistance). For those complicated international itineraries that require custom crafting and significant involvement of our international on-location partners, we do request an intent-to-travel deposit to begin the process. This deposit is applied to the reservations that are made on your behalf.</p>
+            </div>
+            <div>
+                <h3>Where Can I Find My Itinerary Once My Trip is Booked?</h3>
+                <p>Your itinerary as booked by Professional Travel a Direct Travel Company is available on our View Trip website with your specific reservation code as the login. Since we are directly connected to vendors’ inventory systems (airlines, cruise lines, hotels, cars, rail, etc.), you will also be able to retrieve your information on their unique client sites, many of which offer a wealth of information to help you prepare for the trip ahead.</p>
+            </div>
+            <div>
+                <h3>How Do I Cancel a Trip?</h3>
+                <p>Call us right away. If it’s after normal business hours, please contact the next involved travel supplier (airline, hotel, tour operator, cruise line) to cancel any reservations you will not be able to use. If you purchased travel insurance, please contact the insurance company’s 24-hour line to start the cancellation process and ensure your maximum refund.</p>
+            </div>
+        </div>
     );
 };
 
