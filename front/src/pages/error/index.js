e98214f3d873889372d5fe273cf2b47d973237c1
ElodieSayavong <elodie.sayavong.dev@gmail.com>
2022-07-01 16:29:41 +0200

    test deploy

--- front/src/pages/error/index.js
+++ front/src/pages/error/index.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return (
         <div class="notfound">
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
-        <p>The Page you are looking for doesn't exist or an other error occured. Go to <Link to="/">Home Page.</Link></p>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>The Page you are looking for doesn't exist or an other error occured./ Go to <Link to="/">Home Page.</Link></p>
         </div>
     );
 };
