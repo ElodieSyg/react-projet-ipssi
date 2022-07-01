b274ca88512eec288350c3414e227a2f42dcc4c7
unknown <cherifoussama1996@gmail.com>
2022-07-01 15:26:24 +0200

    notfound page

--- front/src/components/router/index.js
+++ front/src/components/router/index.js
@@ -4,10 +4,12 @@ import Home from "../../pages/home/index";
 import Product from "../../pages/product/index";
 import Faq from "../../pages/faq/index";
 import NotFound from "../../pages/error/index";
+import Navbar from "../navbar";
 
 const Router = () => {
     return (
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route axect path="/product" element={<Product />} />
