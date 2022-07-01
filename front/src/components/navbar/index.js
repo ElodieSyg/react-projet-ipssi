474dd91e3d5572bb95b2ba4cfbff1acf11db50f9
unknown <cherifoussama1996@gmail.com>
2022-07-01 12:50:49 +0200

    links

--- front/src/components/navbar/index.js
+++ front/src/components/navbar/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <a>Home</a>
-      <a>FAQ</a>
+    <nav className='navbar'>
+      <Link to="/">Home</Link>
+      <Link to="/faq">FAQ</Link>
     </nav>
   )
 }
